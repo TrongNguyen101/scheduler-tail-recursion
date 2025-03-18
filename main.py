d02371ce3f9fdb37878705a473a7ab9e50b99633
Nguyen Trong <>
2025-03-18 19:40:13 +0700

    update code on main.py

--- main.py
+++ main.py
@@ -24,6 +24,37 @@ days = [
     DayOnWeek("Friday")
 ]
 
+class Timetable:
+    def __init__(self, subjects, timeslots, days):
+        self.subjects = subjects
+        self.timeslots = timeslots
+        self.days = days
+        self.table = [[None for _ in range(len(days))] for _ in range(len(timeslots))]
+
+    def generate_timetable(self):
+        # Example logic to fill the timetable with subjects
+        for i, timeslot in enumerate(self.timeslots):
+            for j, day in enumerate(self.days):
+                if i < len(self.subjects):
+                    self.table[i][j] = self.subjects[i].subjectCode
+                else:
+                    self.table[i][j] = "Free"
+
+    def display_timetable(self):
+        # Print the header
+        col_width = 16
+        print("Time/Day".ljust(col_width), end="")
+        for day in self.days:
+            print(day.day.ljust(col_width), end="")
+        print()
+
+        # Print the timetable
+        for i, timeslot in enumerate(self.timeslots):
+            print(f"{timeslot.start_time}-{timeslot.end_time}", end="\t")
+            for j in range(len(self.days)):
+                print(self.table[i][j], end="\t")
+            print()
+
 print(subjects[0].subjectCode, subjects[0].subjectName)
 print(subjects[1].subjectCode, subjects[1].subjectName)
 print(subjects[2].subjectCode, subjects[2].subjectName)
@@ -42,6 +73,11 @@ print(days[2].day)
 print(days[3].day)
 print(days[4].day)
 
-// Thinking about how to generate a timetable following the above data structure
-// I would create a class called Timetable
-// I would create a method called generate_timetable
+print("\nTimetable")
+timetable = Timetable(subjects, timeslots, days)
+timetable.generate_timetable()
+timetable.display_timetable()
+
+# Thinking about how to generate a timetable following the above data structure
+# I would create a class called Timetable
+# I would create a method called generate_timetable
