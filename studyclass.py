2c133aecdf079e5a0c8931b5361d73c1325f38e4
Nguyen Trong <>
2025-03-18 19:57:15 +0700

    update code

---
+++ studyclass.py
@@ -0,0 +1,8 @@
+# Thinking how to proceed with the above data structure, I would create a class called Timetable
+# I would create a method called generate_timetable
+# I would create a method called display_timetable
+# I would create a method to create class on morning or afternoon
+# Stupid code, out of process
+class StudyClass:
+    def __init__(self, timeStudy):
+        self.timeStudy = timeStudy
